df55a01e6272bcd7d10e7ff6984ac581a0c0b5e7
Nichibotsu <nbesecke05@gmail.com>
2021-10-17 19:16:40 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rekursiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Fakultät(15));
+            Console.ReadKey();
+        }
+        private static Int64 Fakultät(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * Fakultät(n - 1);
+        }
+    }
+}
